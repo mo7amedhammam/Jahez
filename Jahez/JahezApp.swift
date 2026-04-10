909743a0153debab86762f747c1654138ffc8545
mohamed hammam <mohamed.hammam.pro@gmail.com>
2026-04-10 17:21:24 +0200

    Initial Commit

---
+++ Jahez/JahezApp.swift
@@ -0,0 +1,17 @@
+//
+//  JahezApp.swift
+//  Jahez
+//
+//  Created by mohamed hammam on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct JahezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
